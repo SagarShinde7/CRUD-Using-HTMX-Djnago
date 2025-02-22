f4d991c420ca65b971c70313c5ffd345cdc75bd4
Sagar Shinde <106909440+SagarShinde7@users.noreply.github.com>
2025-02-22 16:29:59 +0530

    Add files via upload

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+
+from books.views import (
+    create_book,
+    create_book_form,
+    detail_book,
+    update_book,
+    delete_book
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('htmx/book-form/', create_book_form, name='book-form'),
+    path('htmx/book/<pk>/', detail_book, name='detail-book'),
+    path('htmx/book/<pk>/update/', update_book, name='update-book'),
+    path('htmx/book/<pk>/delete/', delete_book, name='delete-book'),
+    path('<pk>/', create_book, name="create-book")
+]
